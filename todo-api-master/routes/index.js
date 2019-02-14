d74672ae88384c8e5e9796dcbc6f3eddbcc8e1eb
Hardik Furia <36391943+hardikfuria12@users.noreply.github.com>
2019-02-14 02:56:53 -0700

    Update index.js

--- todo-api-master/routes/index.js
+++ todo-api-master/routes/index.js
@@ -6,7 +6,7 @@ var InteractionController = require('../controllers/interaction')
 /* GET home page. */
 
 router.get('/', function(request, response, next){
-  response.render('index', { title: 'Express' })
+  response.render('index', { title: 'Express' })  //gets a request and 'index' is the rendering response to the the get command i think.
 });
 
 router.get('/signup', function(request, response, next){
@@ -21,7 +21,7 @@ router.get('/viz', function(request, response, next){
   response.render('viz', { title: 'Express' })
 });
 
-router.post('/user/add', UserController.addUser);
+router.post('/user/add', UserController.addUser);   //database mein daalneke liye apis.
 router.post('/user/login', UserController.login);
 router.post('/user/logout', UserController.logout);
 router.post('/user/details', UserController.details);
