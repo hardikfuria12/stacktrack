24faac8ddb86da44383a88701fba374214de86c6
Hardik Furia <36391943+hardikfuria12@users.noreply.github.com>
2019-02-14 02:51:03 -0700

    Update app.js

--- todo-api-master/app.js
+++ todo-api-master/app.js
@@ -27,11 +27,11 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
-});
+}); // unsure about what this does.
 
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // joins dirname with views
+app.set('view engine', 'ejs'); // i guess using views
 
 
 app.use(logger('dev'));
@@ -40,8 +40,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index); // routes - index
+app.use('/users', users); // routes - users
 // app.use('/api', api);
 
 
@@ -61,6 +61,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+}); // need to figure how this works.
 
 module.exports = app;
